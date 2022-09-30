24540a6c2905e7f9a52392bad2530dfa3bec2aa6
Saviour Odidi <saviourdavidodidi@gmil.com>
2022-09-30 07:40:54 -0700

    Task. 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef main
+#define main
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int convert_to_integer(char *s);
+int _atoi(char *s);
+int _strlen_recursion(char *s);
+int only_nums(char *s);
+
+#endif
